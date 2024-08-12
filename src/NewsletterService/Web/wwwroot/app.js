bc8fec78a6150cdc7db124bd01f3f771845f7bf9
Sultan Dzjumajev <Sultan.Dzjumajev@norgesgruppen.no>
2024-08-12 23:26:31 +0200

    Fixed vertical scroll

--- src/NewsletterService/Web/wwwroot/app.js
+++ src/NewsletterService/Web/wwwroot/app.js
@@ -1,5 +1,1 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const mainElement = document.querySelector("main");
-    const mainWidth = mainElement.offsetWidth;
-    document.documentElement.style.setProperty('--main-width', `${mainWidth}px`);
-});
+﻿
